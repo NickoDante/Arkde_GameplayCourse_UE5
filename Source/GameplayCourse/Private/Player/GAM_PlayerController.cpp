cc6be9e93488d0912b7a123c168bd32037fd1059
NickoDante <nickodante24@gmail.com>
2026-04-18 12:05:10 -0500

    M2_V4: Enhanced Input System

--- Source/GameplayCourse/Private/Player/GAM_PlayerController.cpp
+++ Source/GameplayCourse/Private/Player/GAM_PlayerController.cpp
@@ -2,3 +2,42 @@
 
 
 #include "Player/GAM_PlayerController.h"
+
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "InputMappingContext.h"
+
+void AGAM_PlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+	
+	UEnhancedInputLocalPlayerSubsystem* InputSubsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	if (!IsValid(InputSubsystem))
+	{
+		return;
+	}
+	
+	for (UInputMappingContext* Context : InputMappingContexts)
+	{
+		InputSubsystem->AddMappingContext(Context, 0);
+	}
+	
+	UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(InputComponent);
+	if (!IsValid(EnhancedInputComponent))
+	{
+		return;
+	}
+	
+	EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ThisClass::Move);
+	EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &ThisClass::Look);
+}
+
+void AGAM_PlayerController::Move()
+{
+	UE_LOG(LogTemp, Warning, TEXT("AGAM_PlayerController::Move()"));
+}
+
+void AGAM_PlayerController::Look()
+{
+	UE_LOG(LogTemp, Warning, TEXT("AGAM_PlayerController::Look()"));
+}
--- Source/GameplayCourse/Public/Player/GAM_PlayerController.h
+++ Source/GameplayCourse/Public/Player/GAM_PlayerController.h
@@ -6,6 +6,9 @@
 #include "GameFramework/PlayerController.h"
 #include "GAM_PlayerController.generated.h"
 
+class UInputMappingContext;
+class UInputAction;
+
 /**
  * 
  */
@@ -13,4 +16,24 @@ UCLASS()
 class GAMEPLAYCOURSE_API AGAM_PlayerController : public APlayerController
 {
 	GENERATED_BODY()
+	
+protected:
+	
+	virtual void SetupInputComponent() override;
+	
+private:
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Arkde|Inputs")
+	TArray<TObjectPtr<UInputMappingContext>> InputMappingContexts;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Arkde|Inputs|Movement")
+	TObjectPtr<UInputAction> MoveAction;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Arkde|Inputs|Movement")
+	TObjectPtr<UInputAction> LookAction;
+	
+	void Move();
+	
+	void Look();
+	
 };
