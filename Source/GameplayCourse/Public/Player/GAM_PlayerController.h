95ff54a4bbc64a11671bbe816a6292cbccb58143
NickoDante <nickodante24@gmail.com>
2026-04-18 12:04:54 -0500

    * M2_V3: Creating Player Controller

---
+++ Source/GameplayCourse/Public/Player/GAM_PlayerController.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "GAM_PlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAMEPLAYCOURSE_API AGAM_PlayerController : public APlayerController
+{
+	GENERATED_BODY()
+};
